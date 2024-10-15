525b9df78ebfec5da3b408a8d72a71fc74e55da6
Muzaffar01-cm <alimovmuzaffar415@gmail.com>
2024-10-15 17:40:26 +0300

    project ready for deploy

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-f+!(10w-6*)%dd6@ou)@%yh)0#z8bp6lu@hkmt1s1#!0!@b9vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://alimovmuzaffar415.pythonanywhere.com"]
 
 
 # Application definition
@@ -104,7 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_T_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
